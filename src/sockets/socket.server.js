884420cc6c0766c7dff45a61e29795a21ae26cf7
patgar-suraj <surajrp7012@gmail.com>
2026-01-05 23:42:50 +0530

    feat: integrate Google GenAI for AI message responses and implement message model
    - Added @google/genai dependency for AI functionalities.
    - Created message model to store chat messages with user and chat references.
    - Implemented ai.service.js to handle AI response generation.
    - Enhanced socket server to authenticate users via JWT and manage AI message interactions.

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,11 +1,67 @@
-const {Server} = require("socket.io")
+const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/users.model");
+const aiServise = require("../services/ai.service")
+const messageModel = require("../model/message.model")
 
-function initSocketServer(httpServer){
-    const io = new Server(httpServer, {})
+function initSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
 
-    io.on("connection", (socket)=>{
-        console.log("new socket connection:", socket.id)
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.token) {
+      next(new Error("authentication faild: valid token required"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+      const user = await userModel.findById(decoded.id);
+
+      socket.user = user;
+      next();
+
+    } catch (error) {
+      next(new Error("authentication faild: valid token required"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+    
+    socket.on("ai-message", async(msgPayload)=>{
+        await messageModel.create({
+            chat: msgPayload.chat,
+            user: socket.user._id,
+            content: msgPayload.content,
+            role: "user"
+        })
+
+        const chatHistroy = await messageModel.find({
+            chat: msgPayload.chat
+        })
+
+        const response = await aiServise.generateResponse(chatHistroy.map((item)=>{
+            return {
+                role: item.role,
+                parts: [{text: item.content}]
+            }
+        }))
+
+        await messageModel.create({
+            chat: msgPayload.chat,
+            user: socket.user._id,
+            content: response,
+            role: "model"
+        })
+
+        socket.emit("ai-response", {
+            content: response,
+            chat: msgPayload.chat
+        })
     })
+  });
 }
 
-module.exports = initSocketServer
+module.exports = initSocketServer;
